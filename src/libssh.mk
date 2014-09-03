b4743512d272636a2a7cfa950acf1e650df25162
Henning Heinold <henning@itconsulting-heinold.de>
2014-09-03 22:29:09 +0200

    libssh: update to version 0.6.3

--- src/libssh.mk
+++ src/libssh.mk
@@ -3,18 +3,18 @@
 
 PKG             := libssh
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.5.5
-$(PKG)_CHECKSUM := e701476ec43f85178bdb36fbb58aa45417a38f5e
+$(PKG)_VERSION  := 0.6.3
+$(PKG)_CHECKSUM := 8189255e0f684d36b7ca62739fa0cd5f1030a467
 $(PKG)_SUBDIR   := libssh-$($(PKG)_VERSION)
-$(PKG)_FILE     := libssh-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://red.libssh.org/attachments/download/51/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc zlib openssl
+$(PKG)_FILE     := libssh-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://red.libssh.org/attachments/download/87/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc zlib openssl pthreads
 
 define $(PKG)_BUILD
     mkdir '$(1)/build'
     cd '$(1)/build' && cmake ../ \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         -DWITH_GCRYPT=OFF \
-        -DWITH_STATIC_LIB=$(if $(BUILD_SHARED),OFF,ON) 
+        -DWITH_STATIC_LIB=$(if $(BUILD_SHARED),OFF,ON)
     $(MAKE) -C '$(1)/build' -j 1 install
 endef
